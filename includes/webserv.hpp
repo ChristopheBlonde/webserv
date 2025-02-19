27f354f247a5fa86b8ae324a394989fb7c3e3d9a
Christophe Blonde <cblonde@student.42.fr>
2025-02-19 17:42:47 +0100

    test

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: cblonde <cblonde@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 07:03:14 by cblonde           #+#    #+#             */
-/*   Updated: 2025/02/19 07:06:00 by cblonde          ###   ########.fr       */
+/*   Updated: 2025/02/19 15:43:16 by cblonde          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,23 @@
 # define WEBSERV_HPP
 
 # include <iostream>
+# include <cstring>
+# include <cerrno>
+# include <csignal>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <poll.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <unistd.h>
 
 # define RED "\e[0;31m"
 # define GREEN "\e[0;32m"
 # define CYAN "\e[0;36m"
 # define RESET "\e[0m"
+
+# define INVALID_SOCKET -1
+# define SOCKET_ERROR -1
+# define PORT 8080
 
 #endif
